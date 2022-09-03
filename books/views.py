ba5307e4bb1656413e2e08095a37bb9eb75c5fbe
Augustine Aroke <arokeaugustine@gmail.com>
2022-09-03 01:55:19 +0100

    Please enter the commit message for your changes. Lines starting

--- books/views.py
+++ books/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework import permissions
 from .models import Author, Book
 from .serializers import AuthorSerializer, BookSerializer
 
@@ -6,6 +7,7 @@ from .serializers import AuthorSerializer, BookSerializer
 class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all().order_by('first_name')
     serializer_class = AuthorSerializer
+    # permission_classes = [permissions.AllowAny]
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all().order_by('name')
