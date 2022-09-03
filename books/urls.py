b8565070c24fc02af467a8abd7b9f03805c584e5
Augustine Aroke <arokeaugustine@gmail.com>
2022-09-03 08:34:03 +0100

    formatted views.py and urls.py

--- books/urls.py
+++ books/urls.py
@@ -1,8 +1,6 @@
 from rest_framework import routers
 from .views import AuthorViewSet, BookViewSet
-from books import views
 from rest_framework.routers import DefaultRouter
-from .models import Author, Book
 
 router = DefaultRouter()
 router.register(r'books', BookViewSet, basename='books')
--- books/views.py
+++ books/views.py
@@ -1,15 +1,14 @@
 from rest_framework import viewsets
-from rest_framework import permissions
 from .models import Author, Book
 from .serializers import AuthorSerializer, BookSerializer
 
 
 class AuthorViewSet(viewsets.ModelViewSet):
-    queryset = Author.objects.all().order_by('first_name')
+    queryset = Author.objects.all()
     serializer_class = AuthorSerializer
-    # permission_classes = [permissions.AllowAny]
+    
 
 class BookViewSet(viewsets.ModelViewSet):
-    queryset = Book.objects.all().order_by('name')
+    queryset = Book.objects.all()
     serializer_class = BookSerializer
 
